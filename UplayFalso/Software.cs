0dd57c4021157d02b8d83029f0b479d166951c25
CryBabyGetCake <talismant03@gmail.com>
2022-04-01 15:50:23 +0200

    Software

---
+++ UplayFalso/Software.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UplayFalso
+{
+    class Software
+    {
+        public string TypeTool;
+        public DateTime Start;
+        public DateTime EndAiring;
+        public List<string> Seasons;
+        public float SeasonsLength;
+    }
+    public class Tools : Software
+    {
+        public string ToolType;
+    }
+    public class Game : Software
+    {
+        public int MetaCritic;
+        public float AveragePlayTime;
+        public string DLC;
+    }
+}

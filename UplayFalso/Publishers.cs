524842661138f5237cfc84c740cff6805b887e37
Arnau Ferreres <arnauferreres1@gmail.com>
2022-04-01 16:08:11 +0200

    Publisher

---
+++ UplayFalso/Publishers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UplayFalso
+{
+    class Publishers
+    {
+        public string CompanyName;
+        public string CompanyCIF;
+        public string Adress;
+        public string WebPage;
+        public string SupportEmail;
+        public List<Icons>;
+    }
+}

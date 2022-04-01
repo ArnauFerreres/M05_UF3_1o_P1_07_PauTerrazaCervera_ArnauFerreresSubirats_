626c59522d89f623e6a859563b36e3db68e384b4
CryBabyGetCake <talismant03@gmail.com>
2022-04-01 16:10:14 +0200

    Audiovisual final

--- UplayFalso/Audiovisual.cs
+++ UplayFalso/Audiovisual.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace UplayFalso
 {
-    class Audiovisual
+    public abstract class Audiovisual
     {
+        public string OriginalFormat;
+        public double Bitrate;
+        public float Duration;
+    }
+
+    public class Videos : Audiovisual
+    {
+        public int Resolution;
+    }
+    public class SoundTracks : Audiovisual
+    {
+        public int AmountTracks;
+        public string NameTracks;
+        public List<string> Episodes;
+        public float SeasonLength;
     }
 }
